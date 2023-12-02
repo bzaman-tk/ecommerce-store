93b1cea493b9782d80d7fd56d74db635f3799aab
bzaman-tk <bzaman.tk@gmail.com>
2023-12-02 08:37:35 +0600

    navbar actions done

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/container";
 import Link from "next/link";
 import MainNav from "@/components/MainNav";
 import getCategories from "@/actions/GetCategories";
+import NavbarActions from "@/components/NavbarActions";
 
 export const revalidate = 0;
 
@@ -15,6 +16,7 @@ const NavBar = async () => {
                         <p className="font-bold text-xl uppercase">Anime Store</p>
                     </Link>
                     <MainNav data={categories} />
+                    <NavbarActions />
                 </div>
             </Container>
         </div>
---
+++ components/ui/Button.jsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
+
+const Button = forwardRef(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+    return (
+        <button
+            className={
+                cn(
+                    "w-auto bg-black rounded-full border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition",
+                    className
+                )
+            }
+            ref={ref}
+        >
+            {children}
+        </button>
+    )
+})
+
+Button.displayName = "Button"
+
+export default Button;
